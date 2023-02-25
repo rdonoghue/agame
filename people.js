b4669bf77009982b7880566a5818023cd3c01eaf
Rob Donoghue <rdonoghue@gmail.com>
2023-02-25 18:45:49 -0500

    made some zoomfixes to people too

--- people.js
+++ people.js
@@ -6,20 +6,26 @@ var cardContainer = document.getElementById("cardcontainer");
 var allCards = cardContainer.innerHTML;
 var navBox = document.getElementById("sidenav");
 
+document.getElementsByTagName("BODY")[0].onresize = function () {
+  checkBottom();
+};
+
 setupPeople();
+checkBottom();
 
 function setupPeople() {
   var allHTML = allCards;
   var navList = navBox.innerHTML;
   for (var k in peopleIndex) {
-    console.log(k);
     allHTML =
       allHTML +
       "<li id='" +
       k +
       "'><img src='" +
       peopleCards[k]["url"] +
-      "' /><div class='cardinfo'><h2>" +
+      "' id='" +
+      k +
+      "Img' /><div class='cardinfo'><h2>" +
       peopleCards[k]["name"] +
       "</h2><h3>" +
       peopleCards[k]["tagline"] +
@@ -28,9 +34,25 @@ function setupPeople() {
       "</p></div></li>";
 
     navList += "<a href='#" + k + "'>" + peopleCards[k]["name"] + "</a>";
-    console.log(navList);
   }
 
   cardContainer.innerHTML = allHTML;
   navBox.innerHTML = navList;
+}
+
+function checkBottom() {
+  let mainBox = document.querySelector("main");
+  let mainBoxInfo = mainBox.getBoundingClientRect();
+  let mainBottom = mainBoxInfo.bottom;
+  for (let k in peopleIndex) {
+    let thisBox = document.getElementById(k + "Img");
+    let boxData = thisBox.getBoundingClientRect();
+    let thisBoxBottom = boxData.bottom;
+    let bottomDist = mainBottom - thisBoxBottom;
+    if (bottomDist < 500) {
+      thisBox.style.transformOrigin = "0 100%";
+    } else {
+      thisBox.style.transformOrigin = "0 0";
+    }
+  }
 }
